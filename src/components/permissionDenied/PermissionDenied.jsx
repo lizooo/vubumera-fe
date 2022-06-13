8acbb08741db3a015cee845761b998f83ea7c741
Yaryna Oprysk <yaryna2705@gmail.com>
2022-06-13 21:33:08 +0300

    created page for not login users

---
+++ src/components/permissionDenied/PermissionDenied.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Button } from 'react-rainbow-components';
+import classes from './PermissionDenied.module.scss';
+import { useNavigate  } from "react-router-dom";
+
+
+const PermissionDenied = () => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/register')
+    }
+
+    return (
+        <div className={classes.wrapper}>
+          <div className={classes.header}>
+            <img className={classes.spark} src="spark.png" />
+            <h1 className={classes.title}>Let's get to know your choice</h1>
+            <img className={classes.home} src="home.png" />
+           <div className={classes.permissionCard}>
+               <img src="wBubbles.png" className={classes.bubbles}/>
+               <div className={classes.infoSection}>
+               <img src="denied.png"/>
+               <div className={classes.text}>
+                   <h5>Great! Almost there!</h5>
+                   <h6>Your the qualified elections is waiting for you </h6>
+               </div>
+               </div>
+               <Button 
+               label="Create Account"
+               onClick={handleClick}
+               className={classes.button}
+               />
+           </div>
+        </div>
+        </div>
+    );
+}
+
+export default PermissionDenied;
